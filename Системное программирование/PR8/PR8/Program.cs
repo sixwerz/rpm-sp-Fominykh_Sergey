4314ae70fc2024f827703ae3140708ecfb7a041e
sixwerz <119935929+sixwerz@users.noreply.github.com>
2026-04-02 11:14:37 +0500

    add pr8

---
+++ Системное программирование/PR8/PR8/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+namespace Server_Name_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string clientfileExe = "C:\\files\\github\\rpm-Fominykh_Sergey\\rpm-sp-Fominykh_Sergey\\Системное программирование\\PR8\\PR8_client\\PR8_client\\bin\\Debug\\net9.0\\PR8_client";
+            Process p = Process.Start(clientfileExe);
+            // Создание именованного канала сервером.
+            NamedPipeServerStream pipestream = new
+            NamedPipeServerStream("Kanal_Name_1");
+            // Ждем соединения с клиентом.
+            Console.WriteLine("Ждем соединения с клиентом");
+            pipestream.WaitForConnection();
+            StreamReader reader = new StreamReader(pipestream);
+            StreamWriter writer = new StreamWriter(pipestream);
+            string ss = "";
+            int fl = 0;
+            while (ss != "ДОМОЙ")
+            {
+                if (fl == 0)
+                {
+                    Console.WriteLine("Ваше сообщение ?");
+                    ss = Console.ReadLine();
+                    writer.WriteLine(ss);
+                    writer.Flush();
+                    fl = 1;
+                }
+                else
+                {
+                    ss = reader.ReadLine();
+                    Console.WriteLine("Получено сообщение от клиента : " + ss);
+                    fl = 0;
+                }
+            }
+            Console.WriteLine("Сеанс связи закончен");
+            Console.ReadLine();
+            // Закрываем канал.
+            pipestream.Close();
+        }
+    }
+}
---
+++ Системное программирование/PR8/PR8_client/PR8_client/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+namespace Klient_Name_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ss;
+            //Создаем именованный канал клиентом.
+            NamedPipeClientStream pipestream = new
+            NamedPipeClientStream("Kanal_Name_1");
+            //Соединяемся с каналом.
+            pipestream.Connect();
+            StreamReader reader = new StreamReader(pipestream);
+            StreamWriter writer = new StreamWriter(pipestream);
+            int fl = 0;
+            do
+            {
+                if (fl == 0)
+                {
+                    ss = reader.ReadLine();
+                    Console.WriteLine("Получено сообщение от сервера: " + ss);
+                    fl = 1;
+                }
+                else
+                {
+                    Console.WriteLine("Сообщение от клиента ?");
+                    ss = Console.ReadLine();
+                    writer.WriteLine(ss);
+                    writer.Flush();
+                    fl = 0;
+                }
+            }
+            while (ss != "ДОМОЙ");
+            Console.WriteLine("Сеанс связи закончен");
+            Console.ReadLine();
+            // Закрываем канал.
+            pipestream.Close();
+        }
+    }
+}
