8ede80f5a1639c3a226b7c514ba0515335b56659
sixwerz <119935929+sixwerz@users.noreply.github.com>
2026-06-04 13:14:10 +0500

    add pr9

---
+++ Системное программирование/PR9/PR9/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PR9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) // Копировать в буфер
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                Clipboard.SetText(textBox1.Text);
+
+                MessageBox.Show("Текст успешно скопирован в буфер обмена!");
+            }
+            else
+            {
+                MessageBox.Show("Введите сначала текст в поле ввода.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsText())
+            {
+                string textFromClipboard = Clipboard.GetText();
+                textBox1.Text = textFromClipboard;
+
+
+                File.WriteAllText("output.txt", textFromClipboard);
+                MessageBox.Show("Текст извлечен и успешно записан в файл");
+            }
+            else
+            {
+                MessageBox.Show("В буфере обмена нет текста! Скопируйте что-нибудь.");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
